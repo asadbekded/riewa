efea2c77128c8d6069d67e41069eb38af75d7f0b
asadbekded <asadbek171@gmail.com>
2022-10-22 10:24:00 +0500

    Add new change

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elBurger = document.querySelector(".burger-btn");
+var elNavbar = document.querySelector(".nav-list");
+
+elBurger.addEventListener("click", function() {
+   elNavbar.classList.toggle("open-nav")
+})
