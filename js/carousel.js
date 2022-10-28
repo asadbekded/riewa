585070f748a62f0f9efcb251278e9f1cb0201ddf
asadbekded <asadbek171@gmail.com>
2022-10-28 19:39:56 +0500

    Add new change

---
+++ js/carousel.js
@@ -0,0 +1,26 @@
+$('.autoplay').slick({
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   arrows: false,
+ });
+
+
+ $('.carousel-happy').slick({
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   arrows: false,
+ });
+
+ $('.partners-carousel').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  rtl: true,
+});
+
