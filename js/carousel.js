b62f2b0059f140f1b639e9e00596667714c61415
asadbekded <asadbek171@gmail.com>
2022-10-29 23:41:08 +0500

    Add new iframe

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,52 @@
 $('.autoplay').slick({
-   slidesToShow: 3,
-   slidesToScroll: 1,
-   autoplay: true,
-   autoplaySpeed: 2000,
-   arrows: false,
- });
-
- $('.carousel-happy').slick({
-   slidesToShow: 4,
-   slidesToScroll: 1,
-   autoplay: true,
-   autoplaySpeed: 2000,
-   arrows: false,
- });
-
- $('.carousel-happy-right').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows: false,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	arrows: false,
 });
 
- $('.partners-carousel').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows: false,
-  rtl: true,
+$('.carousel-happy').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	arrows: false,
+});
+
+$('.carousel-happy-right').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	arrows: false,
+	autoplay: true,
+	infinite: true,
+	autoplaySpeed: 0,
+	slidesToScroll: 1,
+	slidesToShow: 5,
+	arrows: false,
+	cssEase: 'linear',
+	speed: 5000,
+	initialSlide: 1,
+	draggable: false,
+	rtl: true,
+});
+
+$('.partners-carousel').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	arrows: false,
+	autoplay: true,
+	infinite: true,
+	autoplaySpeed: 0,
+	slidesToScroll: 1,
+	slidesToShow: 4,
+	arrows: false,
+	cssEase: 'linear',
+	speed: 5000,
+	initialSlide: 1,
+	draggable: false,
 });
